42418d4aa4a378a075be7f352859a72da75d87b5
Den-Bratchikov <denbratch@gmail.com>
2022-12-20 15:39:19 +0500

    feat: update styles for 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,22 +1,28 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
+import Button from 'components/shared/button/button';
 import Layout from 'components/shared/layout';
-import Link from 'components/shared/link';
 import SEO from 'components/shared/seo';
+import Icon404 from 'icons/icon-404.inline.svg';
 
 // TODO: Add colors and font sizes, check out the reference — https://tailwindui.com/components/marketing/feedback/404-pages#component-5792f8fd3c3c2be236e72c170345a0ce
 //       No need to change anything else. Only colors and font sizes
 const NotFoundPage = () => (
   <Layout>
-    <section className="safe-paddings py-16 sm:py-32">
-      <div className="container px-0">
-        <p className="font-semibold uppercase">404 error</p>
-        <h1>Page not found</h1>
-        <p className="mt-2">Sorry, we couldn’t find the page you’re looking for.</p>
-        <div className="mt-6">
-          <Link to="/">Go back home</Link>
-        </div>
+    <section className="safe-paddings py-52 lg:py-44 md:py-36 sm:py-28 xs:py-10">
+      <div className="container flex flex-col items-center">
+        <Icon404 className="dark:fill-blue dark:text-white sm:w-full" />
+        <h1 className="mt-10 text-center font-mono text-[58px] font-bold leading-none dark:text-white md:text-[48px] sm:mt-5 sm:text-5xl">
+          Ooops! Page not found...
+        </h1>
+        <p className="mt-4 max-w-[592px] text-center font-mono text-xl leading-tight dark:text-white xl:text-md sm:max-w-[480px] sm:text-base">
+          Sorry, the page you are looking for doesn’t exist or has been moved. We suggest you back
+          to home.
+        </p>
+        <Button className="mt-9" size="md" theme="blue" type="button" to="/">
+          Back to Home
+        </Button>
       </div>
     </section>
   </Layout>
