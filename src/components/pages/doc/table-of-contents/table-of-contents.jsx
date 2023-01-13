f5d7b0c7a45993bae2fc318b957a752dcc6f2122
Raman Yasel <raman.yasel@gmail.com>
2023-01-13 09:24:36 +0300

    feat: add back to top button

--- src/components/pages/doc/table-of-contents/table-of-contents.jsx
+++ src/components/pages/doc/table-of-contents/table-of-contents.jsx
@@ -1,12 +1,17 @@
+import { motion } from 'framer-motion';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import EditDocLink from 'components/pages/doc/edit-doc-link';
 import Link from 'components/shared/link';
+import BackToTop from 'icons/back-top.inline.svg';
 import ContentsIcon from 'icons/contents.inline.svg';
 import getIdFromChildren from 'utils/get-id-from-children';
 
 const TableOfContents = ({ items, fileUrl }) => {
+  const stickyTOCRef = useRef(null);
+  const [isButtonShown, setIsButtonShown] = useState(false);
+
   const handleAnchorClick = (e, id) => {
     e.preventDefault();
     document.getElementById(id).scrollIntoView({
@@ -25,9 +30,29 @@ const TableOfContents = ({ items, fileUrl }) => {
     }
   };
 
+  const handleBackToTopClick = (event) => {
+    event.preventDefault();
+
+    document.getElementById('gatsby-focus-wrapper').scrollIntoView({
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    const el = stickyTOCRef.current;
+    const observer = new IntersectionObserver(
+      ([e]) => {
+        setIsButtonShown(e.intersectionRatio < 1);
+      },
+      { rootMargin: '-33px 0px 0px 0px', threshold: [1] }
+    );
+
+    observer.observe(el);
+  }, []);
+
   return (
     <aside className="lg:hidden">
-      <nav className="sticky top-8 my-8 ml-10">
+      <nav className="sticky top-8 my-8 ml-10" ref={stickyTOCRef}>
         <div className="border-b border-solid border-grey-88 pb-2 dark:border-grey-40">
           <EditDocLink to={fileUrl} />
         </div>
@@ -37,7 +62,7 @@ const TableOfContents = ({ items, fileUrl }) => {
               <ContentsIcon className="mb-0.5 text-grey-25 dark:text-grey-88" />
               <h3 className="pl-2 font-mono text-sm font-bold">On this page</h3>
             </div>
-            <ul className="flex flex-col">
+            <ul className="flex flex-col border-b border-solid border-grey-88 pb-5 dark:border-grey-40">
               {items.map(({ title }, index) => {
                 const href = getIdFromChildren(title);
 
@@ -54,6 +79,30 @@ const TableOfContents = ({ items, fileUrl }) => {
                 );
               })}
             </ul>
+            <motion.button
+              className="my-3 font-mono text-sm text-black text-grey-25 transition-colors duration-200 hover:text-blue-light dark:text-white dark:hover:text-blue-dark"
+              initial="from"
+              variants={{
+                from: {
+                  opacity: 0,
+                  transition: {
+                    duration: 0.2,
+                  },
+                },
+                to: {
+                  opacity: 1,
+                  transition: {
+                    duration: 0.2,
+                  },
+                },
+              }}
+              animate={isButtonShown ? 'to' : 'from'}
+              type="button"
+              onClick={handleBackToTopClick}
+            >
+              <BackToTop className="mr-2 inline-flex align-middle" />
+              Back to top
+            </motion.button>
           </>
         )}
       </nav>
