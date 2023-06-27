59fa4bc9c45bcc9281557feb575e132a7ed06cd1
Raman Yasel <raman.yasel@gmail.com>
2023-06-27 16:24:51 +0400

    chore: update currentUrl

--- src/components/shared/seo/seo.jsx
+++ src/components/shared/seo/seo.jsx
@@ -36,7 +36,7 @@ const SEO = ({
   const isRobotsNoindexPage = metaRobotsNoindex === 'noindex';
   const currentTitle = title || opengraphTitle || siteTitle;
   const currentDescription = description || opengraphDescription || siteDescription;
-  const currentUrl = pathname !== '/' ? `${siteUrl}${pathname}` : siteUrl;
+  const currentUrl = pathname !== '/docs/' ? `${siteUrl}${pathname}` : siteUrl;
 
   const opengraphImagePreview =
     opengraphImage && siteUrl + getSrc(opengraphImage.localFile.childImageSharp);
